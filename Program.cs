494b93af638a1e83a8f590574081a2952c2771fe
Eduardo Pires <eduardo.c.pires@outlook.com>
2025-07-03 10:41:18 -0300

    visualizing upgrade http request

--- Program.cs
+++ Program.cs
@@ -7,16 +7,41 @@ app.UseWebSockets();
 
 app.Use(async (context, next) =>
 {
+  WriteRequestParam(context);
+
   if (context.WebSockets.IsWebSocketRequest)
   {
     WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
     Console.WriteLine("WebSocket Connected");
-    return;
   }
-
-  await next();
+  else
+  {
+    Console.WriteLine("Hello from the second request delegate");
+    await next();
+  }
 });
 
 app.MapGet("/", () => "Hello World!");
 
+
+app.Run(async (context) =>
+{
+  Console.WriteLine("Hello from the third request delegate");
+  await context.Response.WriteAsync("Hello from the third request delegate");
+});
+
 app.Run();
+
+void WriteRequestParam(HttpContext context)
+{
+  Console.WriteLine("Request method: {0}", context.Request.Method);
+  Console.WriteLine("Request protocol: {0}", context.Request.Protocol);
+
+  if (context.Request.Headers != null)
+  {
+    foreach (var h in context.Request.Headers)
+    {
+      Console.WriteLine("--> {0}:{1}", h.Key, h.Value);
+    }
+  }
+}
