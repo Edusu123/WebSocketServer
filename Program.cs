b7cfb035fd532e87228f8762df435b4854f0913d
Eduardo Pires <eduardo.c.pires@outlook.com>
2025-07-03 10:13:12 -0300

    websocket middleware

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
+using System.Net.WebSockets;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseWebSockets();
+
+app.Use(async (context, next) =>
+{
+  if (context.WebSockets.IsWebSocketRequest)
+  {
+    WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+    Console.WriteLine("WebSocket Connected");
+    return;
+  }
+
+  await next();
+});
 
 app.MapGet("/", () => "Hello World!");
 
